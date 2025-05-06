e9c9079df1f2a2c8a1611b3e009046b245a1aa68
saharga <saharga@ac.sce.ac.il>
2025-05-06 23:45:52 +0300

    Added bar graph for crimes per peoples only for 2025

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import sqlite3
 import plotly.express as px
+import plotly.graph_objects as go
 import dash
+import requests
 from dash import html, dcc, Output, Input
 import dash_bootstrap_components as dbc
 import json
 
+
 """ --------------------------------------------------- backend ---------------------------------------------------- """
+
+# ----------------------------------------------------- files ----------------------------------------------------------
 
 # Json files for geo_borders (for the map)
 with open("municipalities.geojson", encoding="utf-8") as f:
@@ -16,20 +21,35 @@ with open("districts.geojson", encoding="utf-8") as f:
 # Load police stations CSV
 df_police = pd.read_csv("police_stations.csv")
 
+# ----------------------------------------------- DB and API connection ------------------------------------------------
+
+url = 'https://data.gov.il/api/3/action/datastore_search?resource_id=64edd0ee-3d5d-43ce-8562-c336c24dbc1f&limit=5000'
+response = requests.get(url)
+data = response.json()
+records = data['result']['records']
+df_population = pd.DataFrame.from_records(records)
 conn = sqlite3.connect("crime_2024.db") # DB connection
-query = """ SELECT c.Yeshuv as יישוב,  c.PoliceDistrict as מחוז_משטרתי,
+df_population_clean = df_population[["שם_ישוב", "סהכ"]]
+df_population_clean.to_sql("population", conn, if_exists="replace", index=False)
+
+
+# ------------------------------------------------- queries ------------------------------------------------------------
+
+crime_filters = pd.read_sql_query(""" SELECT c.Yeshuv as יישוב,  c.PoliceDistrict as מחוז_משטרתי,
            c.StatisticGroup as סוג_עבירה, c.Quarter as רבעון, c.Year as שנה,
            COUNT(*) as כמות_פשעים
-    FROM crimes_2024 c
-    WHERE c.Yeshuv IS NOT NULL AND c.StatisticGroup IS NOT NULL
-    GROUP BY c.Yeshuv, c.PoliceDistrict, c.StatisticGroup, c.Quarter, c.Year """
-df = pd.read_sql_query(query, conn) # making df table for reaching data
+           FROM crimes_2024 c
+           WHERE c.Yeshuv IS NOT NULL AND c.StatisticGroup IS NOT NULL
+           GROUP BY c.Yeshuv, c.PoliceDistrict, c.StatisticGroup, c.Quarter, c.Year """, conn)
+
 conn.close()
 
+# ------------------------------------------------ tables --------------------------------------------------------------
+
 quarter_mapping = {"Q1": "ינואר-מרץ","Q2": "אפריל-יוני","Q3": "יולי-ספטמבר","Q4": "אוקטובר-דצמבר"} # quarter types
-df["תיאור_רבעון"] = df["רבעון"].map(quarter_mapping)
-type_options = ["כלל העבירות"] + sorted(df["סוג_עבירה"].dropna().unique())
-quarter_options = ["כל השנה"] + sorted(df["תיאור_רבעון"].dropna().unique())
+crime_filters["תיאור_רבעון"] = crime_filters["רבעון"].map(quarter_mapping)
+type_options = ["כלל העבירות"] + sorted(crime_filters["סוג_עבירה"].dropna().unique())
+quarter_options = ["כל השנה"] + sorted(crime_filters["תיאור_רבעון"].dropna().unique())
 
 """ ---------------------------------------------------------------------------------------------------------------- """
 
@@ -130,6 +150,13 @@ app.layout = html.Div(style={"backgroundColor": "#e6f2ff", "direction": "rtl"}, 
         }),
 
 
+    ]),
+         html.Div(id="crime-ratio-container", style={"display": "none"}, children=[
+              html.H3("10 הערים המובילות ביחס פשיעה לנפש", style={"textAlign": "center"}),
+              dcc.Graph(
+                  id="crime-ratio-graph",
+                  style={"marginTop": "20px"}
+         )
     ])
 ])
 
@@ -137,6 +164,7 @@ app.layout = html.Div(style={"backgroundColor": "#e6f2ff", "direction": "rtl"}, 
 
 @app.callback(
     Output("map-graph", "figure"),
+    Output("crime-ratio-container", "style"),
     Input("year-slider", "value"),
     Input("crime-type-dropdown", "value"),
     Input("quarter-dropdown", "value"),
@@ -147,7 +175,7 @@ app.layout = html.Div(style={"backgroundColor": "#e6f2ff", "direction": "rtl"}, 
 # --------------------------------------------------- Update -----------------------------------------------------------
 
 def update_map(selected_year, selected_crime, selected_quarter, toggle_value, toggle_police):
-    filtered = df[df["שנה"] == selected_year]
+    filtered = crime_filters[crime_filters["שנה"] == selected_year]
     if selected_crime != "כלל העבירות":
         filtered = filtered[filtered["סוג_עבירה"] == selected_crime]
     if selected_quarter != "כל השנה":
@@ -170,7 +198,6 @@ def update_map(selected_year, selected_crime, selected_quarter, toggle_value, to
             zoom=6.5, center={"lat": 31.5, "lon": 34.75}, opacity=0.5
         )
 
-    # Add police stations if selected
     if "stations" in toggle_police:
         fig.add_scattermapbox(
             lat=df_police["lat"], lon=df_police["lon"], mode="markers",
@@ -184,7 +211,47 @@ def update_map(selected_year, selected_crime, selected_quarter, toggle_value, to
         hoverlabel=dict(bgcolor="white", font_size=14),
         title="מפת פשיעה לפי בחירה",
     )
+
+    show_stats_style = {"display": "block"} if selected_year == 2024 else {"display": "none"}
+    return fig, show_stats_style
+
+@app.callback(
+    Output("crime-ratio-graph", "figure"),
+    Input("year-slider", "value")
+)
+def update_crime_ratio_graph(selected_year):
+    if selected_year != 2024:
+        return go.Figure()
+
+    conn = sqlite3.connect("crime_2024.db")
+    crime_per_yeshuv = pd.read_sql("SELECT Yeshuv as יישוב, COUNT(*) as כמות_פשעים FROM crimes_2024 GROUP BY Yeshuv", conn)
+    population = pd.read_sql("SELECT `שם_ישוב` as יישוב, `סהכ` as סהכ_אוכלוסייה FROM population", conn)
+    conn.close()
+    crime_per_yeshuv['יישוב_נקי'] = crime_per_yeshuv['יישוב'].apply(clean_city_name)
+    population['יישוב_נקי'] = population['יישוב'].apply(clean_city_name)
+
+    merged = pd.merge(crime_per_yeshuv, population, left_on='יישוב_נקי', right_on='יישוב_נקי', how='inner')
+    merged["פשיעה_לנפש"] = merged["כמות_פשעים"] / merged["סהכ_אוכלוסייה"]
+    top10 = merged.sort_values(by="פשיעה_לנפש", ascending=False).head(10)
+
+    fig = go.Figure(go.Bar(
+        x=top10["יישוב_נקי"],
+        y=top10["פשיעה_לנפש"],
+        marker_color="indianred"
+    ))
+
+    fig.update_layout(
+        title="יחס פשיעה לנפש - 10 ערים מובילות",
+        xaxis_title="עיר",
+        yaxis_title="פשיעה לנפש",
+        template="simple_white"
+    )
     return fig
+
+def clean_city_name(name):
+    if pd.isna(name):
+        return ""
+    return str(name).strip().replace('-', ' ').replace('"', '').replace("'", '').replace("־", ' ').replace('״', '').replace('׳', '').replace(" ", " ")
 
 """ ---------------------------------------------------------------------------------------------------------------- """
 
